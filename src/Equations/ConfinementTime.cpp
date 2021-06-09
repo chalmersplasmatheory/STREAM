caaa3fa4d3699334ff62661b605ec7e5de9953b6
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-09 16:51:29 +0200

    Added math to the functions

--- src/Equations/ConfinementTime.cpp
+++ src/Equations/ConfinementTime.cpp
@@ -22,34 +22,45 @@ ConfinementTime::ConfinementTime(FVM::UnknownQuantityHandler *u, real_t a, real_
 real_t ConfinementTime::EvaluateConfinementTime(len_t ir){
     real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
-    real_t T_cold = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
     real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD));
 }
 
 real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir){
     real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
-    real_t T_cold = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
     real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return -4/(a*B*I_ref) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD));
 }
 
 real_t ConfinementTime::EvaluateConfinementTime_dIMK2(len_t ir){
     real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
-    real_t T_cold = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
     real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    
+    return 4/(a*B) *Constants::mu0*Constants::mu0*I_MK2/ (M_PI*M_PI*l_MK2*l_MK2) * exp(-I_p/I_ref) * sqrt((T_e+T_i)/((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)*(k_B*Constants::mD)));
 }
 
 real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir){
     real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
-    real_t T_cold = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
     real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return 1/(8*a*a*B) + 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/((T_e+T_i)*(k_B*Constants::mD)));
 }
 
 real_t ConfinementTime::EvaluateConfinementTime_dTi(len_t ir){
     real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
-    real_t T_cold = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
     real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/((T_e+T_i)*(k_B*Constants::mD)));
 }
