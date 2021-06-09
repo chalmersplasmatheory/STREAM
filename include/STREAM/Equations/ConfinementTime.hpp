fdc0dd3eb7a821d952e240c23432a777df744ee5
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-09 10:24:14 +0200

    1/tau first draft hpp-file

---
+++ include/STREAM/Equations/ConfinementTime.hpp
@@ -0,0 +1,35 @@
+#ifndef _STREAM_EQUATION_CONFINEMENT_TIME_HPP
+#define _STREAM_EQUATION_CONFINEMENT_TIME_HPP
+
+#include "FVM/Equation/EvaluableEquationTerm.hpp"
+#include "FVM/UnknownQuantityHandler.hpp"
+/* Behövs båda dessa? Är adresserna rätt?  */
+
+/** 
+ * Implementation of the particle confinement time
+ */
+
+namespace STREAM{
+        class ConefinementTime : public FVM::EvaluableEquationTerm {
+
+                protected:
+                        FVM::UnknownQuantityHandler *unknowns; /* Ska denna vara med? */
+                        
+                        len_t id_Ip, id_Imk2, id_Tcold, id_Ti; 
+
+                public:
+                        ConefinementTime(FVM::UnknownQuantityHandler *u);
+
+                        /* Är real_t rätt data-typ?  */
+                        real_t EvaluateConfinementTime();
+
+                        real_t EvaluateConfinementTime_dIp();
+
+                        real_t EvaluateConfinementTime_dIMK2();
+
+                        real_t EvaluateConfinementTime_dTe();
+
+                        real_t EvaluateConfinementTime_dTi();
+}
+
+#endif/*_STREAM_EQUATION_CONFINEMENT_TIME_HPP */
