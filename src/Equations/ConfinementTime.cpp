09a0e6bfe97324dc645154e9917aa2ea7c9d3039
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-10 10:15:49 +0200

    Added tau with derivatives as comments

--- src/Equations/ConfinementTime.cpp
+++ src/Equations/ConfinementTime.cpp
@@ -38,7 +38,7 @@ real_t ConfinementTime::EvaluateConfinementTime(len_t ir){
 }
 
 /**
- * Evaluates the derivative of inverted confinement time with respect to the plasma current
+ * Evaluates the derivative of the inverted confinement time with respect to the plasma current
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir){
@@ -51,7 +51,7 @@ real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir){
 }
 
 /**
- * Evaluates the derivative of inverted confinement time with respect to the wall current
+ * Evaluates the derivative of the inverted confinement time with respect to the wall current
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dIMK2(len_t ir){
@@ -65,7 +65,7 @@ real_t ConfinementTime::EvaluateConfinementTime_dIMK2(len_t ir){
 }
 
 /**
- * Evaluates the derivative of inverted confinement time with respect to the electron temperature
+ * Evaluates the derivative of the inverted confinement time with respect to the electron temperature
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir){
@@ -78,7 +78,7 @@ real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir){
 }
 
 /**
- * Evaluates the derivative of inverted confinement time with respect to the ion temperature
+ * Evaluates the derivative of the inverted confinement time with respect to the ion temperature
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dTi(len_t ir){
@@ -89,3 +89,73 @@ real_t ConfinementTime::EvaluateConfinementTime_dTi(len_t ir){
     
     return 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/((T_e+T_i)*(k_B*Constants::mD)));
 }
+
+/*
+
+/**
+ * Evaluates the confinement time
+ * /
+
+real_t ConfinementTime::EvaluateConfinementTime(len_t ir){
+    real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD))),-1);
+}
+
+/**
+ * Evaluates the derivative of the confinement time with respect to the plasma current
+ * /
+
+real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir){
+    real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD))),-2)*4/(a*B*I_ref) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD));
+}
+
+/**
+ * Evaluates the derivative of the confinement time with respect to the wall current
+ * /
+
+real_t ConfinementTime::EvaluateConfinementTime_dIMK2(len_t ir){
+    real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    
+    return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD))),-2)*4/(a*B) *Constants::mu0*Constants::mu0*I_MK2/ (M_PI*M_PI*l_MK2*l_MK2) * exp(-I_p/I_ref) * sqrt((T_e+T_i)/((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)*(k_B*Constants::mD)));
+}
+
+/**
+ * Evaluates the derivative of the confinement time with respect to the electron temperature
+ * /
+
+real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir){
+    real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD))),-2)*(1/(8*a*a*B) + 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/((T_e+T_i)*(k_B*Constants::mD))));
+}
+
+/**
+ * Evaluates the derivative of the confinement time with respect to the ion temperature
+ * /
+
+real_t ConfinementTime::EvaluateConfinementTime_dTi(len_t ir){
+    real_t I_p = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_MK2 = unknowns->GetUnknownData(id_Imk2)[ir];
+    real_t T_e = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t T_i = unknowns->GetUnknownData(id_Tcold)[ir];
+    
+    return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+T_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/(k_B*Constants::mD))),-2)*2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_MK2*I_MK2)/((T_e+T_i)*(k_B*Constants::mD)));
+}
+
+*/
