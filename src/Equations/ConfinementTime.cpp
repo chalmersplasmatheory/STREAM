f7d47dc61f0788c8eb992e011abeead76ccd2869
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-14 11:21:20 +0200

    small fixes

--- src/Equations/ConfinementTime.cpp
+++ src/Equations/ConfinementTime.cpp
@@ -17,7 +17,7 @@ ConfinementTime::ConfinementTime(FVM::UnknownQuantityHandler *u, EllipticalRadia
     this->l_MK2=l_MK2;
 
     id_Ip    = unknowns->GetUnknownID(OptionConstants::UQTY_I_P);
-    id_Iwall  = unknowns->GetUnknownID(OptionConstants::UQTY_I_WALL);
+    id_Iwall = unknowns->GetUnknownID(OptionConstants::UQTY_I_WALL);
     id_Tcold = unknowns->GetUnknownID(OptionConstants::UQTY_T_COLD);
     id_Wi    = unknowns->GetUnknownID(OptionConstants::UQTY_WI_ENER);
     id_ni    = unknowns->GetUnknownID(OptionConstants::UQTY_NI_DENS);
@@ -29,14 +29,14 @@ ConfinementTime::ConfinementTime(FVM::UnknownQuantityHandler *u, EllipticalRadia
  */
 
 real_t ConfinementTime::EvaluateConfinementTime(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     return T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD));
 }
@@ -46,14 +46,14 @@ real_t ConfinementTime::EvaluateConfinementTime(len_t ir, real_t t){
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     return -4/(a*B*I_ref) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD));
 }
@@ -63,14 +63,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir, real_t t){
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dIwall(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     
     return 4/(a*B) *Constants::mu0*Constants::mu0*I_wall/ (M_PI*M_PI*l_MK2*l_MK2) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)/((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)*(Constants::mD)));
@@ -81,14 +81,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dIwall(len_t ir, real_t t){
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     return 1/(8*a*a*B) + 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD)));
 }
@@ -98,14 +98,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir, real_t t){
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dWi(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     return 4/3*1/(a*B)*1/n_i * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD)));
 }
@@ -115,14 +115,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dWi(len_t ir, real_t t){
  */
 
 real_t ConfinementTime::EvaluateConfinementTime_dni(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig*/
     
     return -4/3*1/(a*B)*W_i/(n_i*n_i) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD)));
 }
@@ -134,14 +134,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dni(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     return pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-1);
 }
@@ -151,14 +151,14 @@ real_t ConfinementTime::EvaluateConfinementTime(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     return pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-2)*4/(a*B*I_ref) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD));
 }
@@ -168,14 +168,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dIp(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime_dIwall(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     
     return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_ii)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-2)*4/(a*B) *Constants::mu0*Constants::mu0*I_wall/ (M_PI*M_PI*l_MK2*l_MK2) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)/((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)*(Constants::mD)));
@@ -186,14 +186,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dIwall(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-2)*(1/(8*a*a*B) + 2/(a*B) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD))));
 }
@@ -203,14 +203,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dTe(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime_dWi(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-2)*4/3*1/(a*B)*1/n_i * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD)));
 }
@@ -220,14 +220,14 @@ real_t ConfinementTime::EvaluateConfinementTime_dWi(len_t ir, real_t t){
  * /
 
 real_t ConfinementTime::EvaluateConfinementTime_dWi(len_t ir, real_t t){
-    real_t I_p   = unknowns->GetUnknownData(id_Ip)[ir];
+    real_t I_p    = unknowns->GetUnknownData(id_Ip)[ir];
     real_t I_wall = unknowns->GetUnknownData(id_Iwall)[ir];
-    real_t T_e   = unknowns->GetUnknownData(id_Tcold)[ir];
-    real_t W_i   = unknowns->GetUnknownData(id_Wi)[ir];
-    real_t n_i   = unknowns->GetUnknownData(id_ni)[ir];
-    //real_t T_i   = 2/3*W_i/n_i;
-    real_t a     = radials->GetMinorRadius()->Eval(t);
-    real_t B     = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
+    real_t T_e    = unknowns->GetUnknownData(id_Tcold)[ir];
+    real_t W_i    = unknowns->GetUnknownData(id_Wi)[ir];
+    real_t n_i    = unknowns->GetUnknownData(id_ni)[ir];
+
+    real_t a = radials->GetMinorRadius()->Eval(t);
+    real_t B = radials->GetMagneticField()->Eval(t); /*Kanske ändrar sig* /
     
     return -pow((T_e/(8*a*a*B) + 4/(a*B) * exp(-I_p/I_ref) * sqrt((T_e+2/3*W_i/n_i)*(B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/(Constants::mD))),-2)*(-4/3*1/(a*B)*W_i/(n_i*n_i) * exp(-I_p/I_ref) * sqrt((B_v*B_v+Constants::mu0*Constants::mu0/ (M_PI*M_PI*l_MK2*l_MK2)*I_wall*I_wall)/((T_e+2/3*W_i/n_i)*(Constants::mD))));
 }
