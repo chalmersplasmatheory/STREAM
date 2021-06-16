0fe499585d40f5613294e9a129b4fe989b6f0729
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-16 18:43:02 +0200

    Added setDyon() and verifySettings()

--- py/STREAM/Settings/TransportSettings.py
+++ py/STREAM/Settings/TransportSettings.py
@@ -1,6 +1,32 @@
 import numpy as np
-from .. DREAMException import DREAMException
+#from .. DREAMException import DREAMException
 from DREAM.Settings import TransportSettings as DREAMTransport
 
 TRANSPORT_NONE = DREAMTransport.TRANSPORT_NONE
 TRANSPORT_PRESCRIBED = DREAMTransport.TRANSPORT_PRESCRIBED
+TRANSPORT_RECHESTER_ROSENBLUTH = DREAMTransport.TRANSPORT_RECHESTER_ROSENBLUTH
+TRANSPORT_SVENSSON = DREAMTransport.TRANSPORT_SVENSSON
+TRANSPORT_DYON = 5
+
+INTERP3D_NEAREST = DREAMTransport.INTERP3D_NEAREST
+INTERP3D_LINEAR  = DREAMTransport.INTERP3D_LINEAR
+
+INTERP1D_NEAREST = DREAMTransport.INTERP1D_NEAREST
+INTERP1D_LINEAR  = DREAMTransport.INTERP1D_LINEAR
+
+SVENSSON_INTERP1D_PARAM_TIME = DREAMTransport.SVENSSON_INTERP1D_PARAM_TIME
+SVENSSON_INTERP1D_PARAM_IP   = DREAMTransport.SVENSSON_INTERP1D_PARAM_IP
+
+BC_CONSERVATIVE = DREAMTransport.BC_CONSERVATIVE     # Assume no flux through r=rmax
+BC_F_0 = DREAMTransport.BC_F_0                       # Assume f=0 outside the plasma
+BC_DF_CONST = DREAMTransport.BC_DF_CONST             # Assume that df/dr is constant on the plasma boundary
+
+class TransportSettings(DREAMTransport.TransportSettings):
+    def setDYON(self, t=None):
+        self.type = TRANSPORT_DYON
+        
+    def verifySettings(self):
+        if self.type == TRANSPORT_DYON
+            
+        else:
+            super().verifySettings()
