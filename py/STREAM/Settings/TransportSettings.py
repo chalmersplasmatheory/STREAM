7cba76082a8274adab1321ac4f294bf4a333f9fd
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-16 18:08:40 +0200

    Initialized python-settings-file

---
+++ py/STREAM/Settings/TransportSettings.py
@@ -0,0 +1,6 @@
+import numpy as np
+from .. DREAMException import DREAMException
+from DREAM.Settings import TransportSettings as DREAMTransport
+
+TRANSPORT_NONE = DREAMTransport.TRANSPORT_NONE
+TRANSPORT_PRESCRIBED = DREAMTransport.TRANSPORT_PRESCRIBED
