3e1587435c4594d5a5135d1c0447ce4b56cc77ec
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-22 14:48:55 +0200

    Added rebuild, jacobian and derivatives

--- include/STREAM/Equations/NeutralTransport.hpp
+++ include/STREAM/Equations/NeutralTransport.hpp
@@ -5,21 +5,33 @@
 #include "FVM/UnknownQuantityHandler.hpp"
 #include "STREAM/Equations/NeutralInflux.hpp"
 
+//TODO Använda Esmées redan existerande kod för gamma_{n,i}
+
 namespace STREAM {
-	class NeutralTransport { 
+	class NeutralTransport : public DREAM::IonEquationTerm<DREAM::FVM::EquationTerm> { 
 	private:
 		NeutralInflux *NI;
 		PlasmaVolume *PV;
+	    ConfinementTime *CT; 
 		
 		real_t vessel_vol;
 		
+		real_t wall_term;
+		real_t tauinv;
+		
+        real_t dI_p;
+        real_t dI_wall;
+        real_t dT_cold;
+        real_t dW_i;
+        real_t dN_i;
+        
+		
 	public:
-		NeutralTransport(NeutralInflux*, PlasmaVolume*, real_t);
+		NeutralTransport(DREAM::FVM::Grid *g, DREAM::IonHandler *ihdl, const len_t iIon, NeutralInflux*, PlasmaVolume*, ConfinementTime*, real_t);
 		~NeutralTransport();
         
-        real_t GetNeutralTransport();
+        void Rebuild(const real_t, const real_t, FVM::UnknownQuantityHandler*);
         
-        /*
 		virtual bool SetCSJacobianBlock(
             const len_t, const len_t, DREAM::FVM::Matrix*, const real_t*,
             const len_t iIon, const len_t Z0, const len_t rOffset
@@ -30,7 +42,10 @@ namespace STREAM {
         virtual void SetCSVectorElements(
             real_t*, const real_t*, const len_t iIon, const len_t Z0, const len_t rOffset
         ) override;
-        */
+        
+        virtual len_t GetNumberOfNonZerosPerRow() const override{ return 1; }
+        
+        virtual len_t GetNumberOfNonZerosPerRow_jac() const override { return 6; }
 	};
 }
 #endif/*_STREAM_EQUATIONS_NEUTRAL_TRANSPORT_HPP*/
--- src/Equations/NeutralTransport.cpp
+++ src/Equations/NeutralTransport.cpp
@@ -6,14 +6,65 @@ using namespace STREAM;
 /**
  * Constructor.
  */
-NeutralTransport::NeutralTransport(NeutralInflux *NI, PlasmaVolume *PV, real_t vessel_vol) : NI(NI), PV(PV), vessel_vol(vessel_vol) {
+NeutralTransport::NeutralTransport(FVM::Grid *g, IonHandler *ihdl,
+	const len_t iIon, NeutralInflux *NI, PlasmaVolume *PV, ConfinementTime *CT, 
+	real_t vessel_vol) : IonEquationTerm<DREAM::FVM::EquationTerm>(g, ihdl, iIon), 
+	NI(NI), PV(PV), CT(CT), vessel_vol(vessel_vol) {
     SetName("NeutralTransport");
 }
 
-real_t void GetNeutralTransport(real_t t, const len_t iIon){
-    Gamma0 = this->NI->EvaluateNeutralInflux(t, iIon);
-    V_p = PV->GetPlasmaVolume();
-    V_ni= PV->GetNeutralVolume(iIon);
-    
-    return Gamma0/(vessel_vol-V_p+V_ni);
+void NeutralTransport::Rebuild(const real_t t, const real_t, FVM::UnknownQuantityHandler*){
+    real_t Gamma0 = this->NI->EvaluateNeutralInflux(t, iIon);
+    real_t V_p = PV->GetPlasmaVolume();
+    real_t V_ni= PV->GetNeutralVolume(iIon);
+    real_t tauinv = coefftauinv->CT(0);
+    real_t dtauinvdIp    = this->coefftauinv->EvaluateConfinementTime_dIp(0); 
+    real_t dtauinvdIwall = this->coefftauinv->EvaluateConfinementTime_dIwall(0); 
+    real_t dtauinvdTcold = this->coefftauinv->EvaluateConfinementTime_dTcold(0); 
+    real_t dtauinvdWi    = this->coefftauinv->EvaluateConfinementTime_dWi(0); 
+    real_t dtauinvdNi    = this->coefftauinv->EvaluateConfinementTime_dNi(0);
+        
+    this->wall_term = Gamma0/(vessel_vol-V_p+V_ni);
+    this->dI_p      = - wall_term/tauinv * dtauinvdIp;
+    this->dI_wall   = - wall_term/tauinv * dtauinvdIwall;
+    this->dT_cold   = - wall_term/tauinv * dtauinvdTcold;
+    this->dW_i      = - wall_term/tauinv * dtauinvdWi;
+    this->dN_i      = - wall_term/tauinv * dtauinvdNi;
+}
+
+bool IonTransport::SetCSJacobianBlock(
+    const len_t uqtyId, const len_t derivId, FVM::Matrix *jac, const real_t*,
+    const len_t iIon, const len_t, const len_t rOffset
+) {    
+    if(derivId==id_Ip){
+		jac->SetElement(rOffset, 0,this->dI_p);
+		return true;
+	} else if(derivId==id_Iwall){
+		jac->SetElement(rOffset, 0,this->dI_wall);
+		return true;
+	} else if(derivId==id_Tcold){
+		jac->SetElement(rOffset, 0,this->dT_cold);
+		return true;
+	} else if(derivId==id_Wi){
+		jac->SetElement(rOffset, iIon,this->dW_i);
+		return true;
+	} else if(derivId==id_Ni){
+		jac->SetElement(rOffset, iIon,this->dN_i);
+		return true;
+	}
+	else {
+	    return false;
+    }
+}
+void IonTransport::SetCSMatrixElements(
+    FVM::Matrix *mat, real_t*, const len_t, const len_t, const len_t rOffset
+) {
+    mat->SetElement(rOffset, rOffset, wall_term); // Är det en operator som ska verka på en parameter? Vilken parameter?
+} 
+
+
+void IonTransport::SetCSVectorElements(
+    real_t* vec, const real_t*, const len_t iIon, const len_t, const len_t rOffset
+) {
+    vec[rOffset]=wall_term; 
 }
