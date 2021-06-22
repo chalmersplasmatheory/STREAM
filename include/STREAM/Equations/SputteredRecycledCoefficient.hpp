e5106e0e9f55010e3c598697c5f817b39fdb680a
Ida Ekmark <ekmark@student.chalmers.se>
2021-06-22 12:12:06 +0200

    Removed table from class

--- include/STREAM/Equations/SputteredRecycledCoefficient.hpp
+++ include/STREAM/Equations/SputteredRecycledCoefficient.hpp
@@ -8,41 +8,10 @@
 namespace STREAM{
     class SputteredRecycledCoefficient {
         private: 
-            std::unordered_map<len_t /*iIon1*/, std::unordered_map<len_t /*iIon2*/, real_t /*Y*/> > 
-                coefficientTable = { //Är detta korekt syntax för unordered_map?
-                    {/* D */ 1, { //Är detta ett rimligt sätt att göra unordered_map på? Dvs kalla D, C, O för 1, 6, 8? 
-                                {/* C */ 6, 0},
-                                {/* O */ 8, 0}
-                               }
-                    },
-                    {/* C */ 6, {
-                                {/* D */ 1, 0.03},
-                                {/* C */ 6, 0},
-                                {/* O */ 8, 1}
-                               }
-                    },
-                    {/* O */ 8, {
-                                {/* D */ 1, 0},
-                                {/* C */ 6, 0},
-                                {/* O */ 8, 1}
-                               }
-                    } 
-                };
-        /*
-            coefficientTable[ D ] = coefficient_D_row;
-                coefficient_D_row[ C ] = 0;
-                coefficient_D_row[ O ] = 0;
-            coefficientTable[ C ] = coefficient_C_row;
-                coefficient_D_row[ D ] = 0.03;
-                coefficient_D_row[ C ] = 0;
-                coefficient_D_row[ O ] = 1;
-            coefficientTable[ O ] = coefficient_O_row;
-                coefficient_D_row[ D ] = 0;
-                coefficient_D_row[ C ] = 0;
-                coefficient_D_row[ O ] = 1;
-        */
+            std::unordered_map<len_t /*iIon1*/, std::unordered_map<len_t /*iIon2*/, real_t /*Y*/> > *coefficientTable;
+      
         public:
-            SputteredRecycledCoefficient(); // Denna ska vara tom va? Det enda vi vill ha är tabellen
+            SputteredRecycledCoefficient(std::unordered_map<len_t /*iIon1*/, std::unordered_map<len_t /*iIon2*/, real_t /*Y*/> > *coefficientTable);
             void AddSRCoefficient(len_t upper, len_t lower, real_t coefficient);
             real_t GetSRCoefficient(len_t upper, len_t lower);
     };
--- src/Equations/SputteredRecycledCoefficient.cpp
+++ src/Equations/SputteredRecycledCoefficient.cpp
@@ -7,23 +7,23 @@ using namespace std;
 /**
  * Constructor
  */
-SputteredRecycledCoefficient::SputteredRecycledCoefficient(){} // Denna ska vara tom va? Det enda vi vill ha är tabellen
+SputteredRecycledCoefficient::SputteredRecycledCoefficient(std::unordered_map<len_t /*iIon1*/, std::unordered_map<len_t /*iIon2*/, real_t /*Y*/> > *coefficientTable) : coefficientTable(coefficientTable) {}
 
 void SputteredRecycledCoefficient::AddSRCoefficient(len_t upper, len_t lower, real_t coefficient){
-    if (coefficientTable.find(upper) != coefficientTable.end()){ // Ska det vara . innan vissa funktioner?
-        if( coefficientTable.find(upper)->second.find(lower) != coefficientTable.find(upper)->second.end()){ //Är detta rätt sätt att nå den inre unordered_map?
+    if (coefficientTable.find(upper) != coefficientTable.end()){ 
+        if( coefficientTable.find(upper)->second.find(lower) != coefficientTable.find(upper)->second.end()){ 
             coefficientTable.find(upper)->second.erase(lower);
         }
-        coefficientTable.find(upper)->second.insert(pair<len_t,real_t>(lower,coefficient)); //Är detta rätt sätt att lägga till i den inre unordered_map?
+        coefficientTable.find(upper)->second.insert(pair<len_t,real_t>(lower,coefficient)); 
     } else {
         unordered_map<len_t,real_t> innerUM;
         innerUM.insert(pair<len_t,real_t>(lower,coefficient));
-        coefficientTable.insert(pair<len_t, unordered_map<len_t,real_t>>(upper,innerUM));//Är detta rätt sätt att lägga till ett till element??
+        coefficientTable.insert(pair<len_t, unordered_map<len_t,real_t>>(upper,innerUM));
     }
 }
 
 real_t SputteredRecycledCoefficient::GetSRCoefficient(len_t upper, len_t lower){
-    if (coefficientTable.find(upper) != coefficientTable.end() && coefficientTable.find(upper)->second.find(lower) != coefficientTable.find(upper)->second.end()){ // Korrekt?
+    if (coefficientTable.find(upper) != coefficientTable.end() && coefficientTable.find(upper)->second.find(lower) != coefficientTable.find(upper)->second.end()){ 
         return coefficientTable.find(upper)->second.find(lower)->second;
     } else {
         return 0; 
