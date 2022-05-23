8ba7f627aac2e5b6b3331d7fc676101b5d099407
Ida Ekmark <ekmark@student.chalmers.se>
2022-05-23 17:29:19 +0200

    Saved time traces instead of peak value for n_re

--- examples/Deuterium_Burn-through/Runaway_UncertaintySweep/run.py
+++ examples/Deuterium_Burn-through/Runaway_UncertaintySweep/run.py
@@ -152,166 +152,30 @@ def getNrePeak(so, so_init, factor, thresh, toffset=0):
 
 
 
-def drawplot4(axs, so, toffset=0, showlabel=True, save=False, first=True):
+def drawplot4(axs, so, toffset=0, showlabel=True, save=False, first=True, fileaddon='', color='k', linestyle='solid', label=''):
     """
     Draw a plot with a output from the given STREAMOutput object.
     """
     t = so.grid.t[:] + toffset
 
-
-    Te = so.eqsys.T_cold[:, 0]
-    Ti = so.eqsys.W_i.getTemperature()['D'][:, 0]
-
-    ne = so.eqsys.n_cold[:, 0]
-    nD0 = so.eqsys.n_i['D'][0][:]
-    nD1 = so.eqsys.n_i['D'][1][:]
-
-    vth = np.sqrt(2 * e * Te / m_e)
-    streamingParameter = so.eqsys.j_tot[:, 0] / (e * ne * vth)
-
-    Vp = so.other.stream.V_p[:, 0]
-    Poh = -so.other.fluid.Tcold_ohmic[:, 0] * Vp
-    Prad = so.other.fluid.Tcold_radiation[:, 0] * Vp
-    Ptransp = so.other.stream.Tcold_transport[:, 0] * Vp
-    Pequi = so.other.fluid.Tcold_ion_coll[:, 0] * Vp
-
-    # Uext = so.eqsys.V
-    Ures = 2 * np.pi * 5.7 * so.eqsys.E_field[:, 0]
-
-    Ip = so.eqsys.I_p[:, 0]
-    # Ire = e * c * 1.6**2 * np.pi * so.eqsys.n_re[:]
-    Ire = so.eqsys.j_re.current()[:]
-    Iohm = so.eqsys.j_ohm.current()[:]
-    Iwall = so.eqsys.I_wall[:, 0]
-
-    EoverED = so.eqsys.E_field.norm('ED')[1:, 0]
-    ECoverED = so.other.fluid.Eceff[:, 0] / so.other.fluid.EDreic[:, 0]
-
-    tauRE = so.other.stream.tau_RE[:, 0]
-    tauRE1 = so.other.stream.tau_RE1[:, 0]
-    tauRE2 = so.other.stream.tau_RE2[:, 0]
-
-    totD = nD0[1:].flatten() * so.other.stream.V_n_tot['D'][:].flatten() + nD1[1:].flatten() * so.other.stream.V_p[:,
-                                                                                               0].flatten()
-    gammaD = (1 - nD0[1:].flatten() * so.other.stream.V_n_tot['D'][:].flatten() / totD)
-
-    gammaTot = so.other.fluid.runawayRate[:, 0]
-    gammaDreicer = so.other.fluid.gammaDreicer[:, 0]
-    gammaAva = so.other.fluid.GammaAva[:, 0] * so.eqsys.n_re[1:, 0]
+    nre = so.eqsys.n_re[:].flatten()
 
     if save:
         if first:
             worab = 'w'
         else:
             worab = 'ab'
-        t_csv = open('Data/time.csv', worab)
+        t_csv = open('Data/time' + fileaddon + '.csv', worab)
         np.savetxt(t_csv, t)
         t_csv.close()
-        Ip_csv = open('Data/PlasmaCurrent.csv', worab)
-        np.savetxt(Ip_csv, Ip)
-        Ip_csv.close()
-        Ire_csv = open('Data/RunawayCurrent.csv', worab)
-        np.savetxt(Ire_csv, Ire)
-        Ire_csv.close()
-        Iohm_csv = open('Data/OhmicCurrent.csv', worab)
-        np.savetxt(Iohm_csv, Iohm)
-        Iohm_csv.close()
-        Te_csv = open('Data/ElectronTemperature.csv', worab)
-        np.savetxt(Te_csv, Te)
-        Te_csv.close()
-        Ti_csv = open('Data/IonTemperature.csv', worab)
-        np.savetxt(Ti_csv, Ti)
-        Ti_csv.close()
-        EoverED_csv = open('Data/ElectricField.csv', worab)
-        np.savetxt( EoverED_csv,  EoverED)
-        EoverED_csv.close()
-        ECoverED_csv = open('Data/CriticalElectricField.csv', worab)
-        np.savetxt(ECoverED_csv, ECoverED)
-        ECoverED_csv.close()
-        ne_csv = open('Data/ElectronDensity.csv', worab)
-        np.savetxt(ne_csv, ne)
-        ne_csv.close()
+        nre_csv = open('Data/RunawayElectronDensity' + fileaddon + '.csv', worab)
+        np.savetxt(nre_csv, nre)
+        nre_csv.close()
 
-        gammaTot_csv = open('Data/TotalRunawayRate.csv', worab)
-        np.savetxt(gammaTot_csv, gammaTot)
-        gammaTot_csv.close()
-        gammaDreicer_csv = open('Data/DreicerRunawayRate.csv', worab)
-        np.savetxt(gammaDreicer_csv, gammaDreicer)
-        gammaDreicer_csv.close()
-        gammaAva_csv = open('Data/AvalancheRunawayRate.csv', worab)
-        np.savetxt(gammaAva_csv, gammaAva)
-        gammaAva_csv.close()
-        tauRE_csv = open('Data/RunawayConfinementTime.csv', worab)
-        np.savetxt(tauRE_csv, tauRE)
-        tauRE_csv.close()
-        tauRE1_csv = open('Data/RunawayParallellConfinementTime.csv', worab)
-        np.savetxt(tauRE1_csv, tauRE1)
-        tauRE1_csv.close()
-        tauRE2_csv = open('Data/RunawayDriftConfinementTime.csv', worab)
-        np.savetxt(tauRE2_csv, tauRE2)
-        tauRE2_csv.close()
 
-    plotInternal(axs[0, 0], t, Te, ylabel=r'$T$ (eV)', color='k', showlabel=showlabel, label=r'$T_{\rm e}$', yscalelog=True)
-    plotInternal(axs[0, 0], t, Ti, ylabel=r'$T$ (eV)', color='m', showlabel=showlabel, label=r'$T_{\rm i}$', yscalelog=True)
+    plotInternal(axs, t, nre, ylabel=r'$n_{\rm re}$ (m$^{-3}$)', color=color, linestyle=linestyle, showlabel=showlabel, label=label)
 
-    plotInternal(axs[0, 1], t, ne, ylabel=r'$n$ (m$^{-3}$)', color='k', showlabel=showlabel,
-                 label=r'$n_{\rm e}$')
-    plotInternal(axs[0, 1], t, nD0, ylabel=r'$n$ (m$^{-3}$)', color='r', showlabel=showlabel,
-                 label=r'$n_{\rm D0}$')
-    plotInternal(axs[0, 1], t, nD1, ylabel=r'$n$ (m$^{-3}$)', color='b', showlabel=showlabel,
-                 label=r'$n_{\rm D1}$')
-
-    plotInternal(axs[0, 2], t, streamingParameter, ylabel=r'$u_e/v_{\rm th}$', color='k', showlabel=showlabel,
-                 label=r'$\xi$')
-    axs[0, 2].set_ylim([-0.1 * np.max(streamingParameter[100:]), 1.1 * np.max(streamingParameter[100:])])
-
-    plotInternal(axs[1, 0], t[1:], Poh / 1e6, ylabel=r'$P$ (MW)', color='m', showlabel=showlabel, label=r'$P_{\rm oh}$')
-    plotInternal(axs[1, 0], t[1:], Prad / 1e6, ylabel=r'$P$ (MW)', color='r', showlabel=showlabel,
-                 label=r'$P_{\rm rad}$')
-    plotInternal(axs[1, 0], t[1:], Ptransp / 1e6, ylabel=r'$P$ (MW)', color='c', showlabel=showlabel,
-                 label=r'$P_{\rm transp}$')
-    plotInternal(axs[1, 0], t[1:], Pequi / 1e6, ylabel=r'$P$ (MW)', color='g', showlabel=showlabel,
-                 label=r'$P_{\rm equi}$')
-
-    # plotInternal(axs[1, 1], t, Uext, ylabel=r'$U$ (V)', color='k', showlabel=showlabel, label='$U_{\rm ext}$')
-    plotInternal(axs[1, 1], t, Ures, ylabel=r'$U$ (V)', color='m', showlabel=showlabel, label=r'$U_{\rm res}$')
-
-    plotInternal(axs[1, 2], t[1:], tauRE, ylabel=r'$\tau_{\rm re}$ (s)', color='k', showlabel=showlabel,
-                 label=r'$\tau_{\rm re}$', yscalelog=True)
-    plotInternal(axs[1, 2], t[1:], tauRE1, ylabel=r'$\tau_{\rm re}$ (s)', color='r', showlabel=showlabel,
-                 label=r'Parallel', linestyle='--', yscalelog=True)
-    plotInternal(axs[1, 2], t[1:], tauRE2, ylabel=r'$\tau_{\rm re}$ (s)', color='r', showlabel=showlabel,
-                 label=r'Drifts', linestyle=':', yscalelog=True)
-
-    plotInternal(axs[2, 0], t, Ip / 1e6, ylabel=r'$I$ (MA)', color='k', showlabel=showlabel, label=r'$I_{\rm p}$')
-    plotInternal(axs[2, 0], t, Ire / 1e6, ylabel=r'$I$ (MA)', color='m', showlabel=showlabel, label=r'$I_{\rm re}$')
-    plotInternal(axs[2, 0], t, Iohm / 1e6, ylabel=r'$I$ (MA)', color='r', showlabel=showlabel, label=r'$I_{\rm ohm}$')
-    # axs[2, 0].set_ylim([0, 2])
-
-    plotInternal(axs[2, 1], t[1:], EoverED * 100, ylabel=r'$E/E_{\mathrm{D}} (\%)$ ', color='k', showlabel=showlabel,
-                 label=r'$E/E_{\rm D}$', yscalelog=False)
-    plotInternal(axs[2, 1], t[1:], ECoverED * 100, ylabel=r'$E/E_{\mathrm{D}} (\%)$ ', color='m', showlabel=showlabel,
-                 label=r'$E_{\rm C}/E_{\rm D}$', yscalelog=False)
-    axs[2, 1].set_ylim([-0.1*np.max(EoverED[100:]*100), 1.1*np.max(EoverED[100:])*100])
-
-    plotInternal(axs[2, 2], t, Iwall / 1e3, ylabel=r'$I_{\rm wall}$ (kA)', color='k', showlabel=showlabel,
-                 label=r'$I_{\rm wall}$')
-
-    plotInternal(axs[3, 0], t[1:], gammaD, ylabel=r'$\gamma$ (\%)', color='m', showlabel=showlabel,
-                 label=r'$\gamma_{\rm D}$')
-
-    plotInternal(axs[3, 1], t[1:], gammaTot, ylabel=r'$\gamma_{\rm re}$ (s$^{-1}$)', color='k', showlabel=showlabel,
-                 label=r'$\mathrm{d}n_{\rm re} / \mathrm{d} t$', yscalelog=False)
-    plotInternal(axs[3, 1], t[1:], gammaDreicer, ylabel=r'$\gamma_{\rm re}$ (s$^{-1}$)', color='b', showlabel=showlabel,
-                 label=r'$\gamma_{\rm Dreicer}$', yscalelog=False)
-    plotInternal(axs[3, 1], t[1:], gammaAva, ylabel=r'$\gamma_{\rm re}$ (s$^{-1}$)', color='r', showlabel=showlabel,
-                 label=r'$\gamma_{\rm ava}$', yscalelog=False)
-    #axs[3, 1].set_ylim([-0.1*np.max(gammaTot[10:]), 1.1*np.max(gammaTot[10:])])
-
-    for i in range(axs.shape[0]):
-        for j in range(axs.shape[1]):
-            axs[i, j].set_xlim([0, t[-1]])
-            axs[i, j].grid(True)
+    axs.grid(True, color='gainsboro', linewidth=0.5)
 
 
 def plotInternal(ax, x, y, ylabel, xlbl=True, ylim=None, log=False, showlabel=False, label=None, yscalelog = False, *args, **kwargs):
@@ -334,54 +198,46 @@ def plotInternal(ax, x, y, ylabel, xlbl=True, ylim=None, log=False, showlabel=Fa
     if yscalelog:
         ax.set_yscale('log')
 
-def makeplots(so11, so12, save=False):
-    '''
-    fig1, axs1 = plt.subplots(2, 1, figsize=(7,5), sharex=True)
-    drawplot1(axs1, so11, color='b')
-    drawplot1(axs1, so12, color='b', toffset=so11.grid.t[-1])
-    fig2, axs2 = plt.subplots(4, 1, figsize=(7, 10))
-    drawplot2(axs2, so11, color='b')
-    drawplot2(axs2, so12, color='b', toffset=so11.grid.t[-1])
-    fig3, axs3 = plt.subplots(3, 1, figsize=(7, 10))
-    drawplot3(axs3, so11)
-    drawplot3(axs3, so12, toffset=so11.grid.t[-1], showlabel=False)
-    '''
-    fig4, axs4 = plt.subplots(4, 3, figsize=(12, 10))
-    drawplot4(axs4, so11, showlabel=True, save=save)
-    drawplot4(axs4, so12, toffset=so11.grid.t[-1], showlabel=False, save=save, first=False)
+def makeplot(axs, so1, so2, save=False, fileaddon='', color='k', linestyle='solid', label=''):
+    drawplot4(axs, so1, showlabel=True, save=save, fileaddon=fileaddon, color=color, linestyle=linestyle, label=label)
+    drawplot4(axs, so2, toffset=so1.grid.t[-1], showlabel=False, save=save, first=False, fileaddon=fileaddon, color=color, linestyle=linestyle, label=label)
 
-    axs4[0, 0].legend(frameon=False, prop={'size': 10})
-    axs4[0, 1].legend(frameon=False, prop={'size': 10})
-    axs4[1, 0].legend(frameon=False, prop={'size': 10})
-    axs4[2, 0].legend(frameon=False, prop={'size': 10})
-    axs4[2, 1].legend(frameon=False, prop={'size': 10})
-    axs4[1, 2].legend(frameon=False, prop={'size': 10})
-    axs4[3, 1].legend(frameon=False, prop={'size': 10})
 
-    plt.tight_layout()
-    plt.show()
 
 def saveDataArticle():
     pgp = 2 / 133.32 * 8e-5
     nPoints = 100
-    uncertaintyFactors = np.logspace(-3, 0, nPoints)
+    uncertaintyFactors = [1, 0.1, 1e-3]#np.logspace(-3, 0, nPoints)
     nre_peak = np.zeros(nPoints)
     t_thresh = np.zeros(nPoints)
     loss = np.zeros(nPoints)
     loss_ava = np.zeros(nPoints)
-    for f_u, i in zip(uncertaintyFactors, range(nPoints)):
-        ss21 = generate(prefill=pgp, uncertaintyFactor=f_u)
-        ss21.save(f'SweepSettings/settings1_{f_u}.h5')
-        so21 = runiface(ss21, f'SweepSettings/output1_{f_u}.h5', quiet=False)
 
-        ss22 = STREAMSettings(ss21)
-        ss22.fromOutput(f'SweepSettings/output1_{f_u}.h5')
-        ss22.timestep.setTmax(0.15 - ss21.timestep.tmax)
-        ss22.timestep.setNt(1000)
-        ss22.save(f'SweepSettings/settings2_{f_u}.h5')
-        so22 = runiface(ss22, f'SweepSettings/output2_{f_u}.h5', quiet=False)
+    fig, axs = plt.subplots(1, 1, figsize=(5, 5))
+    fileaddons = ['_1', '_0.1', '_0.001']
+    colors = ['tab:blue', 'k', 'tab:red']
+    linestyles = ['solid', 'dashed', 'dotted']
+    labels = ['$f_u=1$', '$f_u=0.1$', '$f_u=0.001$']
+    for f_u, i, fa, c, ls, lab in zip(uncertaintyFactors, range(nPoints), fileaddons, colors, linestyles, labels):
+        print(str(f_u))
+        ss1 = generate(prefill=pgp, uncertaintyFactor=f_u)
+        ss1.save(f'SweepSettings/settings1_{f_u}.h5')
+        so1 = runiface(ss1, f'SweepSettings/output1_{f_u}.h5', quiet=False)
 
-        nre_peak[i], t_thresh[i], loss[i], loss_ava[i] = getNrePeak(so22, so21, f_u, 9e13)
+        ss2 = STREAMSettings(ss1)
+        ss2.fromOutput(f'SweepSettings/output1_{f_u}.h5')
+        ss2.timestep.setTmax(0.15 - ss1.timestep.tmax)
+        ss2.timestep.setNt(1000)
+        ss2.save(f'settings2_{f_u}.h5')
+        so2 = runiface(ss2, f'output2_{f_u}.h5', quiet=False)
+
+        makeplot(axs, so1, so2, save=True, fileaddon=fa, color=c, linestyle=ls, label=lab)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+        #nre_peak[i], t_thresh[i], loss[i], loss_ava[i] = getNrePeak(so2, so1, f_u, 9e13)
+    '''
     plt.semilogx(uncertaintyFactors, nre_peak)
     plt.show()
     plt.semilogx(uncertaintyFactors, t_thresh)
@@ -405,6 +261,7 @@ def saveDataArticle():
     lossava_csv = open('Data/RunawayLossWithAvalanche.csv', "w")
     np.savetxt(lossava_csv, loss_ava)
     lossava_csv.close()
+    '''
 
 def main(argv):
     FONTSIZE = 16
