2c620fb5d7c08d971c163ed263ee826951783462
Ida Ekmark <idaa@ekmark.eu>
2025-08-06 13:49:42 -0400

    Divided by zero because jacWeights was uninitialized, now fixed

--- src/Equations/RadiatedPowerTerm.cpp
+++ src/Equations/RadiatedPowerTerm.cpp
@@ -15,6 +15,10 @@ using namespace STREAM;
 
         this->Volfac = new real_t[ionHandler->GetNzs()];
         this->jacWeights = new real_t[GetNumberOfWeightsElements()];
+        for (len_t iz=0; iz<GetNumberOfWeightsElements();iz++){
+            this->Volfac[iz]=0;
+            this->jacWeights[iz]=0;
+        }
     }
     RadiatedPowerTerm::~RadiatedPowerTerm() {
         delete [] this->jacWeights;
@@ -63,7 +67,7 @@ using namespace STREAM;
             for (len_t iz = 0; iz<ions->GetNZ(); iz++){
                 real_t dVdlambda = volumes->GetNeutralVolume_dLambdai(iz);
 
-                if (jacWeights[iz] != 0)
+                if (jacWeights[iz] > 1e-100)
                     jac->SetElement(iz, 0, jacWeights[iz] * dVdlambda / this->Volfac[iz]);
             }
         }
